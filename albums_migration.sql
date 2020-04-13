f32abc30d6043d0caf51c8ebe0d0656b5e1550a3
Ted Zamarron <ted.zamarron@gmail.com>
2020-04-13 17:40:16 -0500

    done

---
+++ albums_migration.sql
@@ -0,0 +1,11 @@
+USE codeup_test_db;
+DROP TABLE IF EXISTS albums;
+CREATE TABLE albums(
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    artist VARCHAR(100) DEFAULT 'UNKNOWN',
+    name VARCHAR(100) DEFAULT 'UNKNOWN',
+    release_date YEAR,
+    sales FLOAT DEFAULT '0',
+    genre VARCHAR(20) DEFAULT 'UNKNOWN',
+    PRIMARY KEY (id)
+);
