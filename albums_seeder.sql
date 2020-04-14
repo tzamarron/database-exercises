b7a6ec962dd714e44285737dc5498116314d32bf
Ted Zamarron <ted.zamarron@gmail.com>
2020-04-14 10:48:57 -0500

    working

---
+++ albums_seeder.sql
@@ -0,0 +1,12 @@
+USE codeuo_test_db;
+
+INSERT INTO albums (artist,name,release_date,sales,genre)
+VALUES ('Michael Jackson','Thriller',1982,47.3,'Pop/Rock/R&B'),
+('AC/DC','Back in Black',1980,26.1,'Hard Rock'),
+('Pink Floyd','The Dark Side of the Moon',1973,24.2,'Progressive Rock'),
+('Meat Loaf','Bat Out of Hell',1977,21.5,'Hard Rock/Progressive Rock'),
+('Whitney Houston / Various Artists','The Bodyguard',1992,28.4,'R&B/Soul/Pop/Soundtrack'),
+('Eagles','Their Greatest Hits (1971-1975',1976,32.2,'Rock/Soft Rock/ Folk Rock'),
+('Bee Gees / Various Artist','Saturday Night Fever',1977,21.6,'Disco'),
+('Eagles','Their ',1976,32.2,'Rock'),
+('Eagles','Their ',1976,32.2,'Rock'),
