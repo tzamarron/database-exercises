e8579806b4af442740dc92d6b6f215f185499096
Ted Zamarron <ted.zamarron@gmail.com>
2020-04-14 09:33:02 -0500

    review changes

--- albums_migration.sql
+++ albums_migration.sql
@@ -2,10 +2,10 @@ USE codeup_test_db;
 DROP TABLE IF EXISTS albums;
 CREATE TABLE albums(
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    artist VARCHAR(100) DEFAULT 'UNKNOWN',
-    name VARCHAR(100) DEFAULT 'UNKNOWN',
-    release_date YEAR,
-    sales FLOAT DEFAULT '0',
-    genre VARCHAR(20) DEFAULT 'UNKNOWN',
+    artist VARCHAR(150) DEFAULT 'NONE',
+    name VARCHAR(150) NOT NULL,
+    release_date INT(4) UNSIGNED NOT NULL
+    sales FLOAT NOT NULL,
+    genre VARCHAR(500) DEFAULT 'NONE',
     PRIMARY KEY (id)
 );
