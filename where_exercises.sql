984b824f8d52e72e87b88a08c1efa864feecfb8c
Ted Zamarron <ted.zamarron@gmail.com>
2020-04-15 17:09:20 -0500

    modifications

--- where_exercises.sql
+++ where_exercises.sql
@@ -32,4 +32,4 @@ SELECT * FROM employees WHERE last_name LIKE 'E%' AND last_name LIKE '%E';
 SELECT * FROM employees WHERE hire_date LIKE '199%' AND birth_date LIKE '%-12-25';
 
 -- Find all employees with a 'q' in their last name but not 'qu' — 547 rows.
-
+SELECT * FROM employees WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%';
