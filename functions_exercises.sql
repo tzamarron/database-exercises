a0ad3d5dc049c1498a464fa2236347cda98b892a
Ted Zamarron <ted.zamarron@gmail.com>
2020-04-16 11:04:44 -0500

    modifications

--- functions_exercises.sql
+++ functions_exercises.sql
@@ -56,7 +56,12 @@ SELECT * FROM employees WHERE last_name LIKE 'E%' ORDER BY emp_no DESC;
 SELECT * FROM employees WHERE hire_date LIKE '199%' AND birth_date LIKE '%-12-25' ORDER BY birth_date , hire_date DESC;
 
 
+
+
+
+# Update your queries for employees whose names start and end with 'E'. Use concat() to combine their first and last name together as a single column in your results
 SELECT CONCAT(first_name, ' ', last_name)FROM employees WHERE last_name LIKE 'E%E';
 
+# For your query of employees born on Christmas and hired in the 90s, use datediff() to find how many days they have been working at the company
 SELECT CONCAT(first_name, ' ', last_name, 'has been working for ',
        DATEDIFF(curdate(),hire_date), ' days') FROM employees WHERE hire_date LIKE '199%' AND birth_date LIKE '%-12-25';
