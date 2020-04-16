6d69a1fd4cc35772b81f4794bbcebd197cc75958
Ted Zamarron <ted.zamarron@gmail.com>
2020-04-16 09:26:05 -0500

    modifications

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -23,7 +23,7 @@ SELECT * FROM employees WHERE first_name = 'Irena' OR first_name = 'Vidya' OR fi
 SELECT * FROM employees WHERE (first_name = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya') and gender = 'M';
 
 -- Find all employees whose last name starts or ends with 'E' — 30,723 rows.
-SELECT * FROM employees WHERE last_name LIKE 'E%' or last_name LIKE '%E';
+SELECT * FROM employees WHERE (last_name LIKE 'E%') or (last_name LIKE '%E');
 
 -- Duplicate the previous query and update it to find all employees whose last name starts and ends with 'E' — 899 rows.
 SELECT * FROM employees WHERE last_name LIKE 'E%' AND last_name LIKE '%E';
@@ -32,7 +32,7 @@ SELECT * FROM employees WHERE last_name LIKE 'E%' AND last_name LIKE '%E';
 SELECT * FROM employees WHERE hire_date LIKE '199%' AND birth_date LIKE '%-12-25';
 
 -- Find all employees with a 'q' in their last name but not 'qu' — 547 rows.
-
+SELECT * FROM employees WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%';
 
 
 
--- where_exercises.sql
+++ where_exercises.sql
@@ -23,7 +23,7 @@ SELECT * FROM employees WHERE first_name = 'Irena' OR first_name = 'Vidya' OR fi
 SELECT * FROM employees WHERE (first_name = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya') and gender = 'M';
 
 -- Find all employees whose last name starts or ends with 'E' — 30,723 rows.
-SELECT * FROM employees WHERE last_name LIKE 'E%' or last_name LIKE '%E';
+SELECT * FROM employees WHERE (last_name LIKE 'E%') or (last_name LIKE '%E');
 
 -- Duplicate the previous query and update it to find all employees whose last name starts and ends with 'E' — 899 rows.
 SELECT * FROM employees WHERE last_name LIKE 'E%' AND last_name LIKE '%E';
